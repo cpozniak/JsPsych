ec30a0fe6acb1140c795282d7a352f98b7d479d8
Céline Pozniak <cpozniak@tuta.io>
2024-04-03 15:25:17 +0200

    Update speededacceptabilityjudgmentMC.js

--- speededacceptabilityjudgmentMC.js
+++ speededacceptabilityjudgmentMC.js
@@ -44,12 +44,12 @@ var WordByWordPlugin = (function(jspsych) {
             var displayed_word = that.htmlDecode(word).replace(/_/g, ' ');
 
             if (i === position) {
-              stimulus += `<span style="display: inline-block; width: ${displayed_word.length * 14}px;">${displayed_word}</span> `;
+              stimulus += `<span style="font-family: 'Courier New', monospace;">${displayed_word}</span> `;
             } else {
-              stimulus += `<span style="display: inline-block; width: ${word.length * 14}px;">${'_'.repeat(word.length)}</span> `;
+              stimulus += `<span style="font-family: 'Courier New', monospace;">${'_'.repeat(word.length)}</span> `;
             }
           }
-          display_element.innerHTML = "<p style='font-family: Courier, monospace; font-size: 20pt;'>" + stimulus.trim() + "</p>";
+          display_element.innerHTML = "<p style='font-size: 20pt;'>" + stimulus.trim() + "</p>";
           current_position++;
           jsPsych.pluginAPI.setTimeout(() => {
             show_next_word(current_position, this);
